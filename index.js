0130923be88d310192392ac94c85932d3c6f45f0
hm.kim <hm.kim@ohoolabs.com>
2023-04-18 20:40:13 +0900

    feat: index.js 삭제

--- index.js
+++
@@ -1,9 +0,0 @@
-import { drawTextBy2dContext } from "./textRenderBy2d";
-import { drawTextByWeb } from "./textRenderByWeb";
-
-function handleChangeInput() {
-    drawTextBy2dContext();
-    drawTextByWeb();
-}
-
-handleChangeInput();
