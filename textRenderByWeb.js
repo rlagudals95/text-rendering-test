051fb93a0e8640019ef1d4dc8850d1560bfb0834
hm.kim <hm.kim@ohoolabs.com>
2023-04-18 20:37:07 +0900

    feat: web 렌더링 추가

--- textRenderByWeb.js
+++ textRenderByWeb.js
@@ -1,62 +1,46 @@
-function drawTextBy2dContext() {
-  const textInput = document.getElementById('text-input');
-  const fontSelect = document.getElementById('font-select');
-  const fontSizeInput = document.getElementById('font-size-input');
-  const fontWeightInput = document.getElementById('font-weight-input');
-  const letterSpacingInput = document.getElementById('letter-spacing-input');
-  const lineHeightInput = document.getElementById('line-height-input');
-  const textWidth = document.getElementById('text-width');
-  const textHeight = document.getElementById('text-height');
 
-  const text = textInput.value;
-  const font = fontSelect.value;
-  const fontSize = `${fontSizeInput.value}px`;
-  const fontWeight = fontWeightInput.value;
-  const lineHeight = `${lineHeightInput.value * parseFloat(fontSize)}px`;
-  const letterSpacing = `${letterSpacingInput.value}px`;
+const p = document.getElementById('p-text');
+p.style.width = 'auto';
+p.style.height = 'auto';
+p.style.display = 'inline-block';
 
+function drawTextByWeb() {
+  const webTextInput = document.getElementById('text-input');
+  const webFontSelect = document.getElementById('font-select');
+  const webFontSizeInput = document.getElementById('font-size-input');
+  const webFontWeightInput = document.getElementById('font-weight-input');
+  const webLetterSpacingInput = document.getElementById('letter-spacing-input');
+  const webLineHeightInput = document.getElementById('line-height-input');
+  const webTextWidth = document.getElementById('web-text-width');
+  const webTextHeight = document.getElementById('web-text-height');
+
+  const webText = webTextInput.value;
+  const webFont = webFontSelect.value;
+  const webFontSize = `${webFontSizeInput.value}px`;
+  const webFontWeight = webFontWeightInput.value;
+  const webLineHeight = `${webLineHeightInput.value * parseFloat(webFontSize)}px`;
+  const webLetterSpacing = `${webLetterSpacingInput.value}px`;
+  console.log('draw!!', webText, '//')
   // Create a new p element
-  const p = document.createElement('p');
-  p.style.fontFamily = font;
-  p.style.fontSize = fontSize;
-  p.style.fontWeight = fontWeight;
-  p.style.lineHeight = lineHeight;
-  p.style.letterSpacing = letterSpacing;
+
+  p.style.fontFamily = webFont;
+  p.style.fontSize = webFontSize;
+  p.style.fontWeight = webFontWeight;
+  p.style.lineHeight = webLineHeight;
+  p.style.letterSpacing = webLetterSpacing;
   p.style.margin = '0';
   p.style.padding = '0';
-  p.innerText = text;
+  p.innerText = webText;
 
-  // Insert the p element into the document
-  document.body.appendChild(p);
-
+  // Insert the p element into the documente
   // Get the dimensions of the rendered text
   const rect = p.getBoundingClientRect();
   const width = rect.width;
   const height = rect.height;
 
-  // Set the dimensions of the canvas
-  canvas.width = width;
-  canvas.height = height;
-
-  // Clear the canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  // Draw the text onto the canvas
-  ctx.font = `${fontWeight} ${fontSize} ${font}`;
-  ctx.fillStyle = 'black';
-  ctx.textBaseline = 'top';
-  ctx.lineHeight = lineHeight;
-  const lines = text.split('\n');
-  let y = 0;
-  for (const line of lines) {
-    ctx.fillText(line, 0, y);
-    y += parseFloat(lineHeight);
-  }
-
   // Set the text width and height
-  textWidth.innerText = width;
-  textHeight.innerText = height;
-
-  // Remove the p element from the document
-  document.body.removeChild(p);
+  webTextWidth.innerText = width;
+  webTextHeight.innerText = height;
 }
+
+drawTextByWeb();
